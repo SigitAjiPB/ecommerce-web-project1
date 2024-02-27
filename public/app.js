bdfb83c3b63464c400c4f673d085355eeeefbeac
unknown <sigitnj89@gmail.com>
2024-02-27 15:31:34 +0700

    When the smartphone nav button is clicked it brings up a dropdown

--- public/app.js
+++ public/app.js
@@ -10,3 +10,13 @@ const sideBarCloseBtn = document.getElementById('side-bar-close-btn')
 sideBarCloseBtn.addEventListener('click', ()=> {
     headerSideNav.classList.toggle('hidden')
 })
+
+//dropdown smartphone navbar
+const smartphoneNav = document.getElementById('smartphone-nav')
+const smartPhoneNavBtn = document.getElementById('smartphone-nav-a')
+const smartphoneNavContent = document.getElementById('dropdown-smartphone-nav-content')
+
+smartPhoneNavBtn.addEventListener('click', ()=> {
+    smartphoneNavContent.classList.toggle('hidden')
+    smartPhoneNavBtn.classList.toggle('focus:text-orange-500')
+})
